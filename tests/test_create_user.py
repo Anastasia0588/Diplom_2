23bf5e1804986fac48427aa0a22454afa908f73d
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-05-17 20:31:34 +0300

    Задание2. Правки из-за разделения фикстуры на регистрациюи генерацию. Удалены лишние импорты.

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,15 +1,13 @@
 import pytest
-import requests
 import allure
-import urls
-from helper import generate_user_creds, register_user, delete_user
+from helper import register_user, delete_user
 
 
 class TestCreateUser:
 
     @allure.title('Успешное создание уникального пользователя')
-    def test_create_unic_user_success(self):
-        creds = generate_user_creds()
+    def test_create_unic_user_success(self, generate_user):
+        creds = generate_user
         response = register_user(creds)
 
         assert response.status_code == 200
@@ -23,21 +21,24 @@ class TestCreateUser:
         delete_user(access_token)
 
     @allure.title('Создание пользователя, который уже зарегистрирован')
-    def test_create_existed_user_fail(self, user):
-        response = requests.post(urls.register_url, data=user[0])
+    def test_create_existed_user_fail(self, generate_user):
+        response_1 = register_user(generate_user)
+        response_2 = register_user(generate_user)
 
-        assert response.status_code == 403
-        assert response.reason == 'Forbidden'
-        assert response.json()["success"] is False
-        assert response.json()["message"] == "User already exists"
+        assert response_2.status_code == 403
+        assert response_2.reason == 'Forbidden'
+        assert response_2.json()["success"] is False
+        assert response_2.json()["message"] == "User already exists"
+
+        access_token = response_1.json()['accessToken']
+        delete_user(access_token)
 
     @allure.title('Создание пользователя с незаполненным обязательным полем')
     @pytest.mark.parametrize('missed_field', ['email', 'name', 'password'])
-    def test_create_user_with_missed_field_fail(self, missed_field):
-        creds = generate_user_creds()
+    def test_create_user_with_missed_field_fail(self, missed_field, generate_user):
+        creds = generate_user
         del creds[missed_field]
-        response = register_user(creds)
-
+        response = register_user(generate_user)
         assert response.status_code == 403
         assert response.reason == 'Forbidden'
         assert response.json()["success"] is False
